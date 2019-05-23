676cdeaede9cc634959672cc8659a16bb884b731
Loris Gilliand <loris.gilliand@heig-vd.ch>
2019-05-23 16:41:32 +0200

    move buildOrdersName() into Order - Order.buildOrdersName()

--- src/main/java/ch/heigvd/gen/labo4/OrdersWriter.java
+++ src/main/java/ch/heigvd/gen/labo4/OrdersWriter.java
@@ -12,7 +12,7 @@ public class OrdersWriter {
 
         for (int i = 0; i < orders.getOrdersCount(); i++) {
             Order order = orders.getOrder(i);
-            buildOrdersName(sb, order);
+            order.buildOrdersName(sb);
         }
 
         if (orders.getOrdersCount() > 0) {
@@ -20,48 +20,6 @@ public class OrdersWriter {
         }
 
         return sb.append("]}").toString();
-    }
-
-    private void buildOrdersName(StringBuffer sb, Order order) {
-        sb.append("{");
-        sb.append("\"id\": ");
-        sb.append(order.getOrderId());
-        sb.append(", ");
-        sb.append("\"products\": [");
-        for (int j = 0; j < order.getProductsCount(); j++) {
-            Product product = order.getProduct(j);
-            product.getProductContent(sb);
-        }
-
-        if (order.getProductsCount() > 0) {
-            sb.delete(sb.length() - 2, sb.length());
-        }
-
-        sb.append("]");
-        sb.append("}, ");
-    }
-
-    private void getProductContent(StringBuffer sb, Product product) {
-        sb.append("{");
-        sb.append("\"code\": \"");
-        sb.append(product.getCode());
-        sb.append("\", ");
-        sb.append("\"color\": \"");
-        sb.append(product.getColor());
-        sb.append("\", ");
-
-        if (product.getSize() != Product.getSizeNotApplicable()) {
-            sb.append("\"size\": \"");
-            sb.append(product.getSizeFor());
-            sb.append("\", ");
-        }
-
-        sb.append("\"price\": ");
-        sb.append(product.getPrice());
-        sb.append(", ");
-        sb.append("\"currency\": \"");
-        sb.append(product.getCurrency());
-        sb.append("\"}, ");
     }
 
 }
