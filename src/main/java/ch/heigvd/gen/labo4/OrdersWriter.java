4d51ec14335806bb165e6976765b5504eb3dfbe3
Loris Gilliand <loris.gilliand@heig-vd.ch>
2019-05-23 16:24:08 +0200

    extract Orders string creation into method - OrdersWriter.buildOrdersName()

--- src/main/java/ch/heigvd/gen/labo4/OrdersWriter.java
+++ src/main/java/ch/heigvd/gen/labo4/OrdersWriter.java
@@ -12,22 +12,7 @@ public class OrdersWriter {
 
         for (int i = 0; i < orders.getOrdersCount(); i++) {
             Order order = orders.getOrder(i);
-            sb.append("{");
-            sb.append("\"id\": ");
-            sb.append(order.getOrderId());
-            sb.append(", ");
-            sb.append("\"products\": [");
-            for (int j = 0; j < order.getProductsCount(); j++) {
-                Product product = order.getProduct(j);
-                getProductContent(sb, product);
-            }
-
-            if (order.getProductsCount() > 0) {
-                sb.delete(sb.length() - 2, sb.length());
-            }
-
-            sb.append("]");
-            sb.append("}, ");
+            buildOrdersName(sb, order);
         }
 
         if (orders.getOrdersCount() > 0) {
@@ -35,6 +20,25 @@ public class OrdersWriter {
         }
 
         return sb.append("]}").toString();
+    }
+
+    private void buildOrdersName(StringBuffer sb, Order order) {
+        sb.append("{");
+        sb.append("\"id\": ");
+        sb.append(order.getOrderId());
+        sb.append(", ");
+        sb.append("\"products\": [");
+        for (int j = 0; j < order.getProductsCount(); j++) {
+            Product product = order.getProduct(j);
+            getProductContent(sb, product);
+        }
+
+        if (order.getProductsCount() > 0) {
+            sb.delete(sb.length() - 2, sb.length());
+        }
+
+        sb.append("]");
+        sb.append("}, ");
     }
 
     private void getProductContent(StringBuffer sb, Product product) {
