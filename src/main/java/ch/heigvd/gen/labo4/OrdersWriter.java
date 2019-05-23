5efbc8b55e455317483d9d37cbb5f311f4255961
Mateo Tutic <mateo@tutic.ch>
2019-05-23 15:39:00 +0200

    Extract method - OrdersWriter.getContents - Long Method

--- src/main/java/ch/heigvd/gen/labo4/OrdersWriter.java
+++ src/main/java/ch/heigvd/gen/labo4/OrdersWriter.java
@@ -19,27 +19,7 @@ public class OrdersWriter {
             sb.append("\"products\": [");
             for (int j = 0; j < order.getProductsCount(); j++) {
                 Product product = order.getProduct(j);
-
-                sb.append("{");
-                sb.append("\"code\": \"");
-                sb.append(product.getCode());
-                sb.append("\", ");
-                sb.append("\"color\": \"");
-                sb.append(product.getColorFor());
-                sb.append("\", ");
-
-                if (product.getSize() != Product.getSizeNotApplicable()) {
-                    sb.append("\"size\": \"");
-                    sb.append(product.getSizeFor());
-                    sb.append("\", ");
-                }
-
-                sb.append("\"price\": ");
-                sb.append(product.getPrice());
-                sb.append(", ");
-                sb.append("\"currency\": \"");
-                sb.append(product.getCurrency());
-                sb.append("\"}, ");
+                getProductContent(sb, product);
             }
 
             if (order.getProductsCount() > 0) {
@@ -55,6 +35,29 @@ public class OrdersWriter {
         }
 
         return sb.append("]}").toString();
+    }
+
+    private void getProductContent(StringBuffer sb, Product product) {
+        sb.append("{");
+        sb.append("\"code\": \"");
+        sb.append(product.getCode());
+        sb.append("\", ");
+        sb.append("\"color\": \"");
+        sb.append(product.getColorFor());
+        sb.append("\", ");
+
+        if (product.getSize() != Product.getSizeNotApplicable()) {
+            sb.append("\"size\": \"");
+            sb.append(product.getSizeFor());
+            sb.append("\", ");
+        }
+
+        sb.append("\"price\": ");
+        sb.append(product.getPrice());
+        sb.append(", ");
+        sb.append("\"currency\": \"");
+        sb.append(product.getCurrency());
+        sb.append("\"}, ");
     }
 
 }
